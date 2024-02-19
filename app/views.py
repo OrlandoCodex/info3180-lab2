66249891b623463713c6e8189efb535ca3779c91
Orlando Drummonds <orlandodrummonds2015@gmail.com>
2024-02-18 23:25:08 -0500

    Added Profile.html

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from datetime import date
 
 
 ###
@@ -17,6 +18,10 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route('/profile')
+def profile():
+    """Render the website's about page."""
+    return render_template('profile.html', date= date_joined(date.today()))
 
 ###
 # The functions below should be applicable to all Flask apps.
@@ -45,3 +50,6 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+def date_joined( date):
+    return date.strftime("%b, %Y")
